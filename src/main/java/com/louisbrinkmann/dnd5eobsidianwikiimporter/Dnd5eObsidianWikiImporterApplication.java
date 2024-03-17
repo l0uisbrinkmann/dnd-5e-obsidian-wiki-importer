46c8ce1605a688f623d8f79fc002a58aac6881fc
Louis Brinkmann <mail@louisbrinkmann.com>
2024-03-17 21:12:19 +0100

    Created Spring Boot project

---
+++ src/main/java/com/louisbrinkmann/dnd5eobsidianwikiimporter/Dnd5eObsidianWikiImporterApplication.java
@@ -0,0 +1,13 @@
+package com.louisbrinkmann.dnd5eobsidianwikiimporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dnd5eObsidianWikiImporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dnd5eObsidianWikiImporterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/louisbrinkmann/dnd5eobsidianwikiimporter/Dnd5eObsidianWikiImporterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.louisbrinkmann.dnd5eobsidianwikiimporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dnd5eObsidianWikiImporterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
